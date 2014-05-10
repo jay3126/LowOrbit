2ba44717e64aa653d66259a8c088441969dd23bf
Jesal Gadhia <jesal7@gmail.com>
2014-05-09 20:05:21 -0700

    Creating custom stats endpoint

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var http = require('request');
 var port = process.env.PORT || 5000;
 var app = express();
  
 app.get('/', function(request, response) {
     response.sendfile(__dirname + '/index.html');
-}).configure(function() {
+});
+
+app.get('/stats', function(request, response) {
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+	http({uri:'https://api.wheretheiss.at/v1/satellites/25544',json:true}, function (error, res, body) {
+    	if (!error && response.statusCode == 200) {
+    		response.json(body);
+		}
+	});
+});
+
+app.configure(function() {
     app.use('/', express.static(__dirname + '/'));
-}).listen(port);
+});
+
+app.listen(port);
